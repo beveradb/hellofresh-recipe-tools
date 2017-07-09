9529c41a0213e94435fd4f8dcba5127bb472d71d
Andrew Beveridge <github@andrewbeveridge.co.uk>
2017-07-09 21:14:15 +0000

    Focused on headers to try and work around request blocks from CDN

--- fetch-archive.sh
+++ fetch-archive.sh
@@ -3,6 +3,8 @@
 # All of the HelloFresh API requests below require an Authorization header which you can get by making an initial request on the public HelloFresh website and inspecting network requests with DevTools
 # Once you've got one, set it here:
 APIAuthorisationHeader="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MDIyNDQ5MzYsImp0aSI6ImY2YTE1NTM5LWE5Y2UtNDNmMi1iMTllLWEzN2M3ZDkwZDU2YyIsImlhdCI6MTQ5OTYxNTE5MywiaXNzIjoic2VuZiJ9._syKk7fDOEDsvo-x6kbxvUq_WFEzGdw62ccg6RCLJjQ"
+UserAgentHeader="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"
+RefererHeader="https://www.hellofresh.co.uk/?andrews-hello-fresh-scripts"
 
 # Check we're running from a tmux session - you probably want to run this from one!
 if ! { [ "$TERM" = "screen" ] && [ -n "$TMUX" ]; } then
@@ -29,16 +31,16 @@ if [ ! -f all-recipes-search.json ]; then
 	echo -e "Fetching list of recipes from HelloFresh recipe search API using auth header: $APIAuthorisationHeader \n"
 	
 	curl "https://gw.hellofresh.com/api/recipes/search?offset=0&limit=10000&q=&locale=en-GB&country=gb" \
-	-H "origin: https://www.hellofresh.co.uk" \
-	-H "authorization: $APIAuthorisationHeader" \
-	-H "accept-encoding: gzip, deflate, br" \
-	-H "accept-language: en-GB,en;q=0.8,ca;q=0.6" \
-	-H "accept: application/json, text/plain, */*" \
-	-H "pragma: no-cache" \
-	-H "cache-control: no-cache" \
-	-H "authority: gw.hellofresh.com" \
-	-H "referer: https://www.hellofresh.co.uk/recipe-archive/search/?q=andrew" \
-	-H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36" \
+	-H "Origin: https://www.hellofresh.co.uk" \
+	-H "Authorization: $APIAuthorisationHeader" \
+	-H "Accept-Encoding: gzip, deflate, br" \
+	-H "Accept-Language: en-GB,en;q=0.8,ca;q=0.6" \
+	-H "Accept: application/json, text/plain, */*" \
+	-H "Pragma: no-cache" \
+	-H "Cache-Control: no-cache" \
+	-H "Authority: gw.hellofresh.com" \
+	-H "Referer: $RefererHeader" \
+	-H "User-Agent: $UserAgentHeader" \
 	--compressed \
 	-o all-recipes-search.json
 
@@ -59,16 +61,16 @@ cat all-recipes-search.json | jq '.items[].id' | sed 's/"//g' | while read recip
 	echo -e "Fetching recipe data from HelloFresh API for new recipe with ID: $recipeID \n"
 
 	curl "https://gw.hellofresh.com/api/recipes/$recipeID" \
-	-H "origin: https://www.hellofresh.co.uk" \
-	-H "authorization: $APIAuthorisationHeader" \
-	-H "accept-encoding: gzip, deflate, br" \
-	-H "accept-language: en-GB,en;q=0.8,ca;q=0.6" \
-	-H "accept: application/json, text/plain, */*" \
-	-H "pragma: no-cache" \
-	-H "cache-control: no-cache" \
-	-H "authority: gw.hellofresh.com" \
-	-H "referer: https://www.hellofresh.co.uk/recipe-archive/search/?q=andrew" \
-	-H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36" \
+	-H "Origin: https://www.hellofresh.co.uk" \
+	-H "Authorization: $APIAuthorisationHeader" \
+	-H "Accept-Encoding: gzip, deflate, br" \
+	-H "Accept-Language: en-GB,en;q=0.8,ca;q=0.6" \
+	-H "Accept: application/json, text/plain, */*" \
+	-H "Pragma: no-cache" \
+	-H "Cache-Control: no-cache" \
+	-H "Authority: gw.hellofresh.com" \
+	-H "Referer: $RefererHeader" \
+	-H "User-Agent: $UserAgentHeader" \
 	--compressed \
 	-o recipes/$recipeID
   else
@@ -81,11 +83,23 @@ cat all-recipes-search.json | jq '.items[].id' | sed 's/"//g' | while read recip
 	# If we don't already have this recipe webpage cached, download it
 	if [ ! -f webpages/$recipeID.html ]; then
 	        echo -e "Downloading recipe HTML webpage from HelloFresh website for new recipe with ID: $recipeID \n"
-		recipeWebpageURL=`cat recipes/$recipeID | jq '.websiteUrl' | sed 's/"//'`
+		recipeWebpageURL=`cat recipes/$recipeID | jq '.websiteUrl' | sed 's/"//' `
 		
 		if ! [ "$recipeWebpageURL" == "null" ]; then
 			echo "Fetching webpage URL with cURL: $recipeWebpageURL"
-			curl -o webpages/$recipeID.html $recipeWebpageURL
+
+			curl "$recipeWebpageURL" \
+			-H "Connection:keep-alive" \
+			-H "Pragma:no-cache" \
+			-H "Cache-Control:no-cache" \
+			-H "Upgrade-Insecure-Requests:1" \
+			-H "Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" \
+			-H "Accept-Encoding:gzip, deflate" \
+			-H "Accept-Language:en-GB,en;q=0.8,ca;q=0.6" \
+			-H "Referer:$RefererHeader" \
+			-H "User-Agent:$UserAgentHeader" \
+			--compressed \
+			-o webpages/$recipeID.html
 			
 			if [ $(stat -c%s webpages/$recipeID.html) -lt 10000 ]; then
 				echo -e "Webpage fetched, but file webpages/$recipeID.html has size less than 10Kb; an error probably occurred, exiting \n"
@@ -102,11 +116,23 @@ cat all-recipes-search.json | jq '.items[].id' | sed 's/"//g' | while read recip
 	# If we don't already have the recipe card PDF cached, download it
         if [ ! -f cards/$recipeID.pdf ]; then
                 echo -e "Downloading recipe card PDF from cloudfront CDN cardLink for new recipe with ID: $recipeID \n"
-                recipeCardURL=`cat recipes/$recipeID | jq '.cardLink' | sed 's/"//'`
+                recipeCardURL=`cat recipes/$recipeID | jq '.cardLink' | sed 's/"//' `
 
                 if ! [ "$recipeCardURL" == "null" ]; then
                         echo "Fetching card PDF URL with cURL: $recipeCardURL"
-                        curl -o cards/$recipeID.pdf $recipeCardURL
+
+			curl "$recipeCardURL" \
+			-H "Connection:keep-alive" \
+			-H "Pragma:no-cache" \
+			-H "Cache-Control:no-cache" \
+			-H "Upgrade-Insecure-Requests:1" \
+			-H "Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" \
+			-H "Accept-Encoding:gzip, deflate" \
+			-H "Accept-Language:en-GB,en;q=0.8,ca;q=0.6" \
+			-H "Referer:$RefererHeader" \
+			-H "User-Agent:$UserAgentHeader" \
+			--compressed \
+			-o cards/$recipeID.pdf
 
                         if [ $(stat -c%s cards/$recipeID.pdf) -lt 10000 ]; then
                                 echo -e "Card PDF fetched, but file cards/$recipeID.pdf size less than 10Kb; an error probably occurred, exiting \n"
